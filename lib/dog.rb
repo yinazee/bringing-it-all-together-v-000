ca2b0a6a1bc00492c0c01e56c45e90028ccf795a
Yina Zhu <yinagit@gmail.com>
2018-04-23 18:29:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,5 +22,16 @@ class Dog
    DB[:conn].execute(sql)
  end
 
-
+ def self.save
+   if self.id
+     self.update
+   else
+     sql = <<-SQL
+     INSERT INTO dogs (name, breed)
+     VALUES (?, ?)
+     SQL
+     DB[:conn].execute(sql, self.name, self.breed)
+     @id: = DB[:conn].execute("SELECT las_insert_rowid() FROM dogs").flatten[0]
+   end
+   self
 end

8e7a33159e27e21708427ae77ad39cbeee697f27
Yina Zhu <yinagit@gmail.com>
2018-04-23 19:47:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,13 +65,15 @@ class Dog
 
    DB[:conn].execute(sql, id).map do |row|
      self.new_from_db(row)
-  end.first
-end
+   end.first
+ end
 
-def self.update
-  sql "UPDATE dog SET name = ?, breed = ? WHERE id = ?"
+  def self.update
+    sql "UPDATE dog SET name = ?, breed = ? WHERE id = ?"
 
-  DB[:conn].execute(sql, self.name, self.breed, self.id)
-end
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+  
 
 end

b3349ae113338f2cf4d45f4a9a303f8b3221c926
Yina Zhu <yinagit@gmail.com>
2018-04-23 19:17:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,7 +50,8 @@ class Dog
    "SELECT * FROM dogs WHERE id = ? LIMIT 1"
    SQL
 
-   DB[:conn].execute(sql, id).map do |row| self.new_from_db(row)}
+   DB[:conn].execute(sql, id).map do |row|
+     self.new_from_db(row)
   end.first
 end
 

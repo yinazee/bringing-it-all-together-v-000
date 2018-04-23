12e58aa18b4974796e09a82b696799f9990b51e0
Yina Zhu <yinagit@gmail.com>
2018-04-23 20:10:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -95,11 +95,6 @@ class Dog
 
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-
-
-
-    DB[:conn].execute(sql, id).map do |row|
-      self.new_from_db(row)
     end.first
   end
 

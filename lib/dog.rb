cb7ad34a6048f8ce111baa0d629d332044ffb320
Yina Zhu <yinagit@gmail.com>
2018-04-23 18:24:39 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+  attr_accessor :id, :name, :breed
+
+  def initialize(hash)
+    hash.each {|key, value| self.send(("#{key}="), value)}
+  end
+
+  def self.create_table
+   sql =  <<-SQL
+     CREATE TABLE IF NOT EXISTS dogs (
+       id INTEGER PRIMARY KEY,
+       name TEXT,
+       breed TEXT
+       )
+   SQL
+   DB[:conn].execute(sql)
+ end
+
+
+
+
+end

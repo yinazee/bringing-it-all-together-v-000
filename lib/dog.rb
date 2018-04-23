01f0b0e058715b13d049e966cad08d5533cefce1
Yina Zhu <yinagit@gmail.com>
2018-04-23 19:58:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,6 +79,8 @@ class Dog
     if !dog.empty?
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(hash)
   end
 
 end

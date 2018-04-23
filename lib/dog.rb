40ee69314d014cafc501f4f52f4399e23d5b2f5a
Yina Zhu <yinagit@gmail.com>
2018-04-23 20:03:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -82,8 +82,8 @@ class Dog
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
       dog = self.create(name: name, breed: breed)
+    end
+    dog
   end
-  dog
-end
 
 end
